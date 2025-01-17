f243b883a85b31573d4e8ed322f6e59edd71e577
Gleb Kapustin <gleb.kapustin@outlook.com>
2025-01-17 23:51:09 +0300

    Добавление чистой архитектуру, моделей

---
+++ Core/Models/User.cs
@@ -0,0 +1,24 @@
+﻿namespace DevelopForum.Core.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public List<Post>? Posts { get; set; }
+
+
+
+
+            
+
+
+
+
+
+
+    }
+}
